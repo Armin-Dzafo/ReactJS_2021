9d1b79ed07c0e041f6e4187aa1d4f31bb831ce6b
alojsb <alojsbirmingham@gmail.com>
2021-12-09 01:40:50 +0100

    added modal and backdrop

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function deleteHandler() {
+    
+}
+
+function Todo(props) {
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+                <button className="btn" onClick={deleteHandler}>Delete</button>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
